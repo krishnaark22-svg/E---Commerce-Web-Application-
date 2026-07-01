755e40d6e69b037f835ddc19df1e19204ae1b8f3
Krishna Priya U.P <krishnaark22@gmail.com>
2026-07-01 20:07:16 +0530

    Add Orders API with POST and GET endpoints

---
+++ Orders.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+let orders = [];
+
+router.post("/", (req, res) => {
+  orders.push(req.body);
+  res.json({ message: "Order placed" });
+});
+
+router.get("/", (req, res) => res.json(orders));
+
+module.exports = router;
